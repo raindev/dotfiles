005947053b122c12337f91bd982c6943a3b15fc1
Andrew Barchuk <andrew@raindev.io>
2022-10-23 13:01:17 +0200

    Replace deprecated LSP formatting function

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -35,7 +35,7 @@ local on_attach = function(_, bufnr)
   vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
 require('lspconfig').rust_analyzer.setup{
