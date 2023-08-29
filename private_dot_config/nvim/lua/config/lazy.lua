380cc79f2f3d8581ad42a155e362f45bc25953b3
Andrew Barchuk <andrew@raindev.io>
2023-08-29 10:45:34 +0200

    Work around a LazyVim clangd LSP issue
    And remove Python language support

--- private_dot_config/nvim/lua/config/lazy.lua
+++ private_dot_config/nvim/lua/config/lazy.lua
@@ -18,7 +18,6 @@ require("lazy").setup({
       { import = "lazyvim.plugins.extras.lang.clangd" },
       { import = "lazyvim.plugins.extras.lang.go" },
       { import = "lazyvim.plugins.extras.lang.java" },
-      { import = "lazyvim.plugins.extras.lang.python" },
       { import = "lazyvim.plugins.extras.lang.rust" },
       { import = "lazyvim.plugins.extras.lang.typescript" },
       { import = "lazyvim.plugins.extras.lang.yaml" },
