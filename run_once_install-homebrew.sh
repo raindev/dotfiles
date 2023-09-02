299c26bf96a56b31ef757003a34bc2d19e6d1199
Andrew Barchuk <andrew@raindev.io>
2023-09-02 20:54:05 +0200

    Combine installing Homebrew with its packages
    There doesn't seem to be a guarantee of run_once scripts running before
    run_onchange scripts

--- run_once_install-homebrew.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# vi: set ft=bash
-set -e
-
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
--- run_onchange_install-homebrew-packages.sh
+++ run_onchange_install-homebrew-packages.sh
@@ -2,6 +2,10 @@
 # vi: set ft=bash
 set -e
 
+if ! command -v brew; then
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
 brew install \
 	bash-completion \
 	exercism \
