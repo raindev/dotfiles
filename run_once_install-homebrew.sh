4f827d18f27359444e55039fd52cc03154257769
Andrew Barchuk <andrew@raindev.io>
2023-09-02 19:55:42 +0200

    Separate shared Homebrew packages

--- run_once_install-homebrew.sh
+++ run_once_install-homebrew.sh
@@ -1,1 +1,4 @@
+#!/usr/bin/env bash
+# vi: set ft=bash
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
---
+++ run_onchange_install-homebrew-packages.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# vi: set ft=bash
+set -e
+
+brew install \
+	bash-completion \
+	exercism \
+	go \
+	neovim \
+	staticcheck \
+	topgrade \
+	watchexec
