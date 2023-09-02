ab76ee8adb186e7cfe2870f02814ffbb53c92e54
Andrew Barchuk <andrew@raindev.io>
2023-09-02 20:38:51 +0200

    Only set up pass on private desktops

--- run_once_install-homebrew.sh
+++ run_once_install-homebrew.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # vi: set ft=bash
+set -e
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
--- run_once_set-up-pass.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [ ! -e "$HOME/.password-store" ]; then
-	echo '>setting up pass'
-	git clone git@github.com:raindev/passwords.git\
-		"$HOME/.password-store"
-	pass git init
-fi
