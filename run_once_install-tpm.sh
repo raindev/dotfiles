e33baa6c6205376f1b72f5e2dbfdb11dd1f596d6
Andrew Barchuk <andrew@raindev.io>
2023-12-26 18:38:45 +0100

    Make TPM install script idempotent

--- run_once_install-tpm.sh
+++ run_once_install-tpm.sh
@@ -1,4 +1,6 @@
 #!/bin/env bash
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-~/.tmux/plugins/tpm/bin/install_plugins
+if ! [[ -d ~/.tmux/plugins/tpm ]]; then
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+	~/.tmux/plugins/tpm/bin/install_plugins
+fi
