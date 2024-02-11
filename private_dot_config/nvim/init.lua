a0c67417e8b7fd33342d12ba23e2e7d6cf3b9ab2
Andrew Barchuk <andrew@raindev.io>
2024-02-11 21:55:28 +0100

    nvim: present dashboard after lazy.nvim

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -238,24 +238,39 @@ require('lazy').setup({
    {
       'nvimdev/dashboard-nvim',
       event = 'VimEnter',
-      opts = {
-         theme = 'doom',
-         config = {
-            week_header = {
-               enable = true
-            },
-            center = {
-               { action = 'Telescope find_files', desc = ' Find file', icon = ' ', key = 'f' },
-               { action = 'ene | startinsert', desc = ' New file', icon = ' ', key = 'n' },
-               { action = 'Telescope oldfiles', desc = ' Recent files', icon = ' ', key = 'r' },
-               { action = 'Telescope live_grep', desc = ' Find text', icon = ' ', key = 'g' },
-               { action = 'edit ~/.config/nvim/init.lua', desc = ' Config', icon = ' ', key = 'c' },
-               { action = 'lua require("persistence").load()', desc = ' Load Session', icon = ' ', key = 's' },
-               { action = 'lua require("persistence").load({last = true})', desc = ' Last Session', icon = '▶︎ ', key = 'l' },
-               { action = 'qa', desc = ' Quit', icon = ' ', key = 'q' },
-            },
+      opts = function()
+         local opts = {
+            theme = 'doom',
+            config = {
+               week_header = {
+                  enable = true
+               },
+               center = {
+                  { action = 'Telescope find_files', desc = ' Find file', icon = ' ', key = 'f' },
+                  { action = 'ene | startinsert', desc = ' New file', icon = ' ', key = 'n' },
+                  { action = 'Telescope oldfiles', desc = ' Recent files', icon = ' ', key = 'r' },
+                  { action = 'Telescope live_grep', desc = ' Find text', icon = ' ', key = 'g' },
+                  { action = 'edit ~/.config/nvim/init.lua', desc = ' Config', icon = ' ', key = 'c' },
+                  { action = 'lua require("persistence").load()', desc = ' Load Session', icon = ' ', key = 's' },
+                  { action = 'lua require("persistence").load({last = true})', desc = ' Last Session', icon = '▶︎ ', key = 'l' },
+                  { action = 'qa', desc = ' Quit', icon = ' ', key = 'q' },
+               },
+            }
          }
-      },
+
+         -- close Lazy and re-open when the dashboard is ready
+         if vim.o.filetype == "lazy" then
+            vim.cmd.close()
+            vim.api.nvim_create_autocmd("User", {
+               pattern = "DashboardLoaded",
+               callback = function()
+                  require("lazy").show()
+               end,
+            })
+         end
+
+         return opts
+      end,
       dependencies = { { 'nvim-tree/nvim-web-devicons' } }
    },
    {
