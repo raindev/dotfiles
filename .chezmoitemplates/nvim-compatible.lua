c9b90ee3c49ec1f940d17023c2f4eae9acf1d1d7
Andrew Barchuk <andrew@raindev.io>
2025-04-26 09:43:27 +0200

    Configure indentation for Python in nvim

--- .chezmoitemplates/nvim-compatible.lua
+++ .chezmoitemplates/nvim-compatible.lua
@@ -488,6 +488,15 @@ vim.api.nvim_create_autocmd({ 'FileType' }, {
 })
 vim.api.nvim_create_autocmd({ 'FileType' }, {
    group = augroup,
+   pattern = { 'python' },
+   callback = function()
+      vim.opt_local.expandtab = true
+      vim.opt_local.softtabstop = 4
+      vim.opt_local.shiftwidth = 4
+   end
+})
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+   group = augroup,
    pattern = { 'sh' },
    callback = function()
       vim.opt_local.expandtab = true
