71a5ea6517bc947ca3143bf69bc4200efb33cfd0
raindev <andrew@raindev.io>
2026-03-07 16:48:11 +0100

    Remove no longer used symlink for time logs

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -7,5 +7,7 @@ function on_fish_prompt --on-event fish_prompt
 end
 
 # bun
-set --global --export BUN_INSTALL "$HOME/.bun"
-set --global --export --prepend PATH $BUN_INSTALL/bin
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+fish_add_path /Users/abarchuk/.pyenv/versions/3.11.11/bin
