e4a654ee1681cef6b4ba5e81443c3718af580416
Andrew Barchuk <andrew.barchuk@zalando.de>
2026-04-22 13:49:52 +0200

    Source local fish configuration

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -7,7 +7,7 @@ function on_fish_prompt --on-event fish_prompt
 end
 
 # bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
+set --global --export BUN_INSTALL "$HOME/.bun"
+fish_add_path $BUN_INSTALL/bin
 
-fish_add_path /Users/abarchuk/.pyenv/versions/3.11.11/bin
+source $__fish_config_dir/config.local.fish 2>/dev/null
